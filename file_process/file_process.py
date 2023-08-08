234e7fa4378633023c8350a032cab25ae0946215
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2023-08-07 20:28:38 -0400

    process txt, code, json, pdf files

--- file_process/file_process.py
+++ file_process/file_process.py
@@ -4,6 +4,27 @@ from typing import Callable
 from fastapi import UploadFile
 from ..base import BaseTool
 
+def process_txt_file(file_contents: bytes, word_limit, cut=True):
+    all_text = file_contents.decode("utf-8", errors="ignore")
+    all_words = all_text.split(" ")
+    if len(all_words) > word_limit:
+        all_words = all_text[:word_limit]
+        all_text = " ".join(all_words)
+    return all_text
+
+def process_pdf_file(file_contents: bytes, word_limit):
+    pdfReader = PyPDF2.PdfReader(io.BytesIO(file_contents))
+
+    # Extract text from each page
+    all_text = ""
+    for page in range(len(pdfReader.pages)):
+        pageObj = pdfReader.pages[page]
+        text = pageObj.extract_text()
+        all_text += text + "\n"
+        if len(all_text.split(" ")) > word_limit:
+            break
+    return all_text
+
 class FileProcessTool(BaseTool):
     name: str = "file_process"
     description: str = "Tool for process files to improve the discussion"
@@ -11,7 +32,7 @@ class FileProcessTool(BaseTool):
     usable_by_bot = False
 
     def __init__(self, func: Callable=None, **kwargs):
-        self.word_limit = kwargs.get("pdf_word_limit", 1000)
+        self.word_limit = kwargs.get("file_word_limit", 1000)
         self.current_user_files = {}
         OnStartUp = kwargs.get("OnStartUp")
         OnStartUpMsgEnd = kwargs.get("OnStartUpMsgEnd")
@@ -27,19 +48,18 @@ class FileProcessTool(BaseTool):
     
     async def handle_file_upload(self, user_id, file: UploadFile):
         try:
-            file_contents = await file.read() # binary value of the file
-            pdfReader = PyPDF2.PdfReader(io.BytesIO(file_contents))
+            print(f"Upload file: user id: {user_id}, file name: {file.filename}, file header: {file.headers}")
 
-            # Extract text from each page
-            all_text = ""
-            for page in range(len(pdfReader.pages)):
-                pageObj = pdfReader.pages[page]
-                text = pageObj.extract_text()
-                all_text += text + "\n"
-                if len(all_text.split(" ")) > self.word_limit:
-                    break
+            file_contents = await file.read() # binary value of the file
+            if file.headers["content-type"] == "text/plain": # txt file
+                all_text = process_txt_file(file_contents, self.word_limit) 
+            elif file.headers["content-type"] == "application/json" or file.headers["content-type"].split("/")[0] == "text": # json file or scripts
+                all_text = process_txt_file(file_contents, self.word_limit, False)
+            elif file.headers["content-type"] == "application/pdf": # pdf file
+                all_text = process_pdf_file(file_contents, self.word_limit)
+            else:
+                raise Exception("Unsupported file type. We accept plain text, json or pdf files")
             self.current_user_files[user_id] = all_text
-            print(f"Upload file: user id: {user_id}, file name: {file.filename}")
             return {"user_id": user_id, "filename": file.filename, "status": "success"}
         except Exception as e:
             print(e)
