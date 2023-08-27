b6f93c9c3765b0cd51338703306b284dec4d8c01
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2023-08-27 11:39:09 -0400

    Implement knn, GPT embedding for similarity check

--- memory/memory.py
+++ memory/memory.py
@@ -24,6 +24,7 @@ from ..base import BaseTool
 
 dotenv.load_dotenv()
 openai.api_key = os.environ["OPENAI_KEY"]
+os.environ["OPENAI_API_KEY"] = os.environ["OPENAI_KEY"] # for langchain compatibility
 
 DEFAULT_USER_FACT = {
     "name": None,
@@ -40,9 +41,15 @@ DEFAULT_USER_FACT = {
 
 
 class Memory:
-    def __init__(self, db: BaseClient=None, short_memory_length: int=10, model="gpt-3.5-turbo-16k"):
+    def __init__(self, db: BaseClient=None, short_memory_length: int=10, 
+                 model="gpt-3.5-turbo-16k", embedding_type="gpt", history_limit=3):
         '''
             short_memory_length: length of conversation considered for the short memory. This is the number of user messages. if short_memory_length=1, the short memory will actually include at least 3 messages (1 initial message from AI, 1 from user, 1 from the AI assistant.)
+
+            embedding_type can be chosen from "gpt" for OpenAI embedding or "st" sentence transformer embedding,
+            For now, st may be incompatible, due to different embedding size.
+
+            history_limit: what's the maximum number of answers for a query to consider for repetitiveness
         '''
         if db == None:
             raise Exception("database is None when initializing memory")
@@ -57,6 +64,32 @@ class Memory:
             self.threshold = 28000
         else:
             self.threshold = 2000
+
+        self.history_limit = history_limit
+
+        if embedding_type == "gpt":
+            embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_KEY"])
+        else:
+            try:
+                import torch
+                model_kwargs = {"device": "cuda"} if torch.cuda.is_available() else {"device": "cpu"}
+            except:
+                model_kwargs = {"device": "cpu"}
+            embeddings = SentenceTransformerEmbeddings(
+                model_name="sentence-transformers/all-mpnet-base-v2", 
+                model_kwargs=model_kwargs)
+
+        pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment=os.environ["PINECONE_API_ENV"])
+        index_name = "fileidx"
+        dimension = 1536
+        # uncomment these for real production, slow check
+        # try:
+        #     if index_name not in pinecone.list_indexes():
+        #         print("Creating pinecone index")
+        #         pinecone.create_index(index_name, dimension)
+        # except Exception as e:
+        #     print(e)
+        self.pinecone_db = Pinecone(index=pinecone.Index(index_name), embedding=embeddings, text_key="text")
 
     def store_user_facts(self, user_id, facts):
         user_fact_ref = self.memory.document(user_id)
@@ -206,14 +239,126 @@ class Memory:
         #     await self.update_memory(user_id)
         #     print("memory about user updated")
     
-    def retrieve_responses_from_similar_queries(self, user_id, user_assistants):
+    def restrict_tokens(self, closest_interactions):
+        i = 0
+        token_counts = 0
+        while i < len(closest_interactions):
+            token_counts += len(closest_interactions[i][0].split(" ")) + len(closest_interactions[i][1].split(" ")) + 2
+            if token_counts > 200:
+                # in case we go over token limit, keep only around 200 tokens
+                break
+            i += 1
+        closest_interactions = closest_interactions[:i+1]
+
+        # construct a proper history for GPT to understand
+        hist = []
+        for i in range(len(closest_interactions)):
+            hist.append({"role": "user", "content": closest_interactions[i][0]})
+            hist.append({"role": "assistant", "content": closest_interactions[i][1]})
+        return hist
+
+    def retrieve_interactions_from_similar_queries(self, user_id, user_assistants):
         '''For now, we simply use a tf-idf vectorizer and knn search'''
         if user_assistants is None or len(user_assistants) == 0:
             return
         user_current_session = self.active_user_sessions[user_id]
         assert user_current_session[-1]["role"] == "user"
-        user_msgs = {msg["content"] : idx for idx, msg in enumerate(user_current_session) if msg["role"] == "user"}
+        user_msgs = [(msg["content"], user_current_session[idx+1]["content"]) \
+                     for idx, msg in enumerate(user_current_session) \
+                     if msg["role"] == "user" and idx < len(user_current_session) - 1]
+        user_msgs.append((user_current_session[-1]["content"], ""))
 
+        start_time = time.time()
+        vectorizer = TfidfVectorizer(encoding="utf-8", decode_error="ignore", stop_words="english")
+        X = vectorizer.fit_transform([m[0] for m in user_msgs]).toarray()
+        print(f"Shape: {X.shape}, Time taken to vectorize all the data: {time.time() - start_time}")
+
+        start_time = time.time()
+        nn_finder = NearestNeighbors(n_neighbors=5)
+        nn_finder.fit(X)
+        print(f"Time taken to find nn: {time.time() - start_time}")
+
+        dist, neigh = nn_finder.kneighbors([X[-1]], min(5, len(user_msgs)))
+        dist = dist.reshape(-1)
+        neigh = neigh.reshape(-1)
+        # the values should already be sorted in increasing order of distance
+        closest_interactions = []
+        for i in range(len(dist)):
+            if neigh[i] == len(user_msgs) - 1:
+                # do not use the latest message
+                continue
+            interaction = user_msgs[neigh[i]]
+            print(f"With sentence: {interaction[0]}, distance: {dist[i]}")
+            if 0 <= dist[i] < 0.9:
+                closest_interactions.append(interaction)
+            if len(closest_interactions) > self.history_limit:
+                # retrieve at most self.history_limit interactions
+                break
+        
+        if len(closest_interactions) == 0:
+            return
+        
+        hist = self.restrict_tokens(closest_interactions)
+        # inject to the bots
+        info = f"Do not repeat yourself with the following interactions: {json.dumps(hist)}"
+        for assistant in user_assistants:
+            assistant(info)
+
+    def insert_vector_store(self, user_id):
+        '''This should be used when the response from GPT is finished'''
+        user_current_session = self.active_user_sessions[user_id]
+        assert len(user_current_session) >= 2
+        assert user_current_session[-1]["role"] == "assistant"
+        assert user_current_session[-2]["role"] == "user"
+        query = user_current_session[-2]["content"]
+        answer = user_current_session[-1]["content"]
+        id = query.replace(" ", "-")
+        metadata = {
+            "answers": [answer]
+        }
+
+        # check if the same query exists already
+        # if exists, append the answer to the previous id
+        # otherwise, insert the query
+        start_time = time.time()
+        search_results = self.pinecone_db.similarity_search_with_score(query, k=self.history_limit, namespace=f"{user_id}-history")
+        for (doc, score) in search_results:
+            if doc.page_content == query:
+                doc.metadata["answers"].append(answer)
+                while len(doc.metadata["answers"]) > self.history_limit:
+                    doc.metadata["answers"].pop(0)
+                self.pinecone_db.delete(ids=[id], namespace=f"{user_id}-history")
+                metadata = doc.metadata
+                break
+        self.pinecone_db.add_texts(texts=[query], metadatas=[metadata], ids=[id], namespace=f"{user_id}-history")
+        print(f"Time to search similar and insert query: {query} :: {time.time() - start_time}")
+
+    def retrieve_interactions_from_similar_queries_vector_store(self, user_id, user_assistants):
+        '''In this function, we use Pinecone vector store, with either sentence transformer or OpenAI embeddings'''
+        user_current_session = self.active_user_sessions[user_id]
+        assert len(user_current_session) >= 1
+        assert user_current_session[-1]["role"] == "user"
+        query = user_current_session[-1]["content"]
+        start_time = time.time()
+        search_results = self.pinecone_db.similarity_search_with_score(query, k=self.history_limit, namespace=f"{user_id}-history")
+
+        closest_interactions = []
+        for (doc, score) in search_results:
+            if score < 0.85:
+                continue
+            for ans in doc.metadata["answers"]:
+                if len(closest_interactions) < self.history_limit:
+                    closest_interactions.append((query, ans))
+        print(f"Time to search similar: {time.time() - start_time}, similar queries: {closest_interactions}")
+
+        if len(closest_interactions) == 0:
+            return
+        
+        hist = self.restrict_tokens(closest_interactions)
+        # inject to the bots
+        info = f"Do not repeat yourself with the following interactions: {json.dumps(hist)}"
+        for assistant in user_assistants:
+            assistant(info)
         
 class MemoryTool(BaseTool):
     name: str = "memory"
@@ -224,7 +369,11 @@ class MemoryTool(BaseTool):
         db = kwargs.get("db", None)
         short_memory_length = kwargs.get("short_memory_length", 5)
         memory_model = kwargs.get("memory_model", "gpt-3.5-turbo-16k")
-        self.memory = Memory(db=db, short_memory_length=short_memory_length, model=memory_model)
+        memory_embedding_type = kwargs.get("memory_embedding_type", "gpt")
+        memory_history_limit = kwargs.get("memory_history_limit", 3)
+        self.memory = Memory(db=db, short_memory_length=short_memory_length, model=memory_model,
+                             embedding_type=memory_embedding_type,
+                             history_limit=memory_history_limit)
         # All the handlers must have been correctly setup, otherwise Memory is no use, 
         # so if there is any error, we must raise
         OnStartUp = kwargs.get("OnStartUp")
@@ -266,7 +415,8 @@ class MemoryTool(BaseTool):
         user_assistants = kwargs.get("user_assistants", [])
         if user_tool_settings[self.name]:
             self.memory.update_user_session(user_id, message)
-            self.memory.retrieve_responses_from_similar_queries(user_id, user_assistants)
+            self.memory.retrieve_interactions_from_similar_queries(user_id, user_assistants)
+            # self.memory.retrieve_interactions_from_similar_queries_vector_store(user_id, user_assistants)
             # mem = self.get_memory(user_id)
             # for k, v in mem.items():
             #     user_info[k] = v
@@ -278,6 +428,7 @@ class MemoryTool(BaseTool):
         message = kwargs.get("message")
         if user_tool_settings[self.name]:
             self.memory.update_user_session(user_id, message)
+            # self.memory.insert_vector_store(user_id)
 
     def OnUserDisconnected(self, **kwargs):
         user_tool_settings = kwargs.get("user_tool_settings", {self.name: False})
